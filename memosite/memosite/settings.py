1f9089ba860be8b6566ab5598edf77610dfa0942
Guillermo Sebastian Carcamo Diaz (IMAGEMAKER) <guillermocarcamo.imagemaker@latam.com>
2023-12-05 18:55:48 -0300

    update allowed hosts

--- memosite/memosite/settings.py
+++ memosite/memosite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-bumth&=irtm(xd4=stsl1wh!+&k)h^15jglicrc=bxf83q@^!=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.91.101.238']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
